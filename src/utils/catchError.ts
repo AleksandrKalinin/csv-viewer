d20dd23e02f85b6d682ed47ac5d37bc55aeaba6e
Aleksandr Kalinin <1995kalininaleksandr@gmail.com>
2023-12-18 11:42:15 +0100

    refactor: add minor refactoring

--- src/utils/catchError.ts
+++ src/utils/catchError.ts
@@ -6,11 +6,5 @@ const toast = useToast()
 
 export const catchError = (error: AxiosError | Error) => {
   const axiosError = error as AxiosError
-  // const status = axiosError.response?.status;
   toast.error(axiosError.message)
-  // if (status) {
-  //   toast.error(i18Instance.global.t(`errorCodes.${status}`));
-  // } else {
-  //   toast.error(axiosError.message);
-  // }
 }
